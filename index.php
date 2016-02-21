a6962ed2f7998b90f3ebe659d533515f1a6c546f
hideokamoto <hide.okamoto@digitalcube.jp>
2016-02-21 15:36:12 +0900

    add is_admin css

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes();?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
@@ -8,7 +8,7 @@
 		* { max-width: 100%;height: auto;}
 		</style>
 	</head>
-	<body>
+	<body <?php body_class() ;?>>
 		<div id="app" class="fullHeight">
 		</div>
 		<?php wp_footer();?>
